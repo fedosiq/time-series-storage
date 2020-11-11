d7f4d60e47fc0f484a121875653c73243472e5e1
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2020-11-11 22:07:59 +0300

    kafka: add second node

--- producer.py
+++ producer.py
@@ -3,9 +3,14 @@ from json import dumps
 from kafka import KafkaProducer
 import time
 import random
+import logging
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092',
-                         value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:19092',
+    acks="all", # Receive acks from all the Kafka followers
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
 
 for e in range(1000):
     timenow = int(time.time())
@@ -14,5 +19,6 @@ for e in range(1000):
         "timestamp": timenow,
         "data": e
     }
-    producer.send('test', value=data)
+    producer.send('telemetry', value=data)
+    print("data sent")
     sleep(1)

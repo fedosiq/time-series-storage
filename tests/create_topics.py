5eec80e3e7d7ca3670e5c373289c3603314bf486
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2021-01-13 20:42:59 +0300

    tmp

--- tests/create_topics.py
+++ tests/create_topics.py
@@ -9,7 +9,7 @@ import logging
 def create_topic(num_partitions=3):
     a = AdminClient({"bootstrap.servers": "localhost:19092"})
 
-    telemetry = [NewTopic("telemetry", num_partitions=num_partitions, replication_factor=1)]
+    telemetry = [NewTopic("telemetry", num_partitions=num_partitions, replication_factor=2)]
 
     fs = a.create_topics(telemetry)
 

118916b4f1bfe5aaed8c6071ddbdccd865f9af4b
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2021-01-14 03:05:33 +0300

    remove unused files

--- create_topics.sh
+++
@@ -1,6 +0,0 @@
-docker run wurstmeister/kafka bash -c " 
-    kafka-topics.sh --create \
-        --zookeeper host.docker.internal:2181 \
-        --topic telemetry \
-        --partitions 2 \
-        --replication-factor 2"
